aa3a72c63929ed4e83250f51a25486e7799b370b
cynthiawjy135 <s225138694@deakin.edu.au>
2026-05-13 20:20:19 +1000

    Added failing TDD test for ontrack function

---
+++ src/test/java/tdd_testing/submissionDueTest.java
@@ -0,0 +1,29 @@
+package tdd_testing;
+
+import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+
+public class submissionDueTest {
+
+	 @Test
+	    public void lateSubmissionShouldReturnTrue() {
+
+	        Assert.assertTrue(
+	            SubmissionChecker.isLate(
+	                "2026-05-11",
+	                "2026-05-10"
+	            )
+	        );
+	    }
+
+	    @Test
+	    public void onTimeSubmissionShouldReturnFalse() {
+
+	        Assert.assertFalse(
+	            SubmissionChecker.isLate(
+	                "2026-05-09",
+	                "2026-05-10"
+	            )
+	        );
+	    }
+}
